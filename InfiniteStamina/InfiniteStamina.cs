ef9fb10279a4045ded19e6d31101dd3cb757b5ad
Mathias Dumain <mathias.dumain@edu.univ-fcomte.fr>
2025-02-28 18:58:54 +0100

    V1.0.0 Release

---
+++ InfiniteStamina/InfiniteStamina.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using InfiniteStamina.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteStamina
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class InfiniteStamina : BaseUnityPlugin
+    {
+        private const string modGUID = "Galaxcito.REPO.InfiniteStamina";
+        private const string modName = "Infinite Stamina";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        internal ManualLogSource mls;
+
+        internal static InfiniteStamina instance { get; private set; }
+
+
+        void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this;
+            }
+
+            mls = Logger;
+
+            mls.LogInfo("The mod Infinite Stamina by Galaxcito was loaded successfully :p");
+
+            harmony.PatchAll(typeof(InfiniteStamina));
+            harmony.PatchAll(typeof(PlayerStaminaPatch));
+        }
+
+    }
+}
---
+++ InfiniteStamina/Patches/PlayerStaminaPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteStamina.Patches
+{
+    [HarmonyPatch(typeof(PlayerController))]
+    class PlayerStaminaPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPatch("FixedUpdate")]
+        [HarmonyPostfix]
+
+        static void InfiniteStaminaPatch(PlayerController __instance)
+        {
+
+
+            if (__instance.sprinting)
+            {
+                __instance.EnergyCurrent = __instance.EnergyStart;
+            }
+            else if (__instance.Sliding)
+            {
+                __instance.EnergyCurrent = __instance.EnergyStart;
+            }
+        }
+    }
+}
+    
